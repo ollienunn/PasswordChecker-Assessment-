3d2248ef110d3f43e71679152b9c8b44c885a352
OLLIE <onunn@student.saintaug.nsw.edu.au>
2025-05-16 11:58:31 +1000

    reorganised and made the password input larger to fit better on the screen and look nice

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ def toggle_mask(event): # Toggles the password
 
 ######################
 
-
-
 ######################
 # Need a score system for the password,
 #                                       speacial characters = + points
@@ -29,17 +27,28 @@ app = gp.GooeyPieApp("Password Checker") # Defines the stuff in app
 app.set_size(WIDTH, HEIGHT) # Makes the app a certain size depending on the width and height
 app.set_grid(ROWS, COLUMNS) # Makes a grid
 
-intro_lbl = gp.StyleLabel(app, "Password Checker 9000") # Label
-intro_lbl.font_size = 20 # Font size
-password_lbl = gp.Label(app, "Enter your password") # Label
-password_inp = gp.Secret(app) # Makes the input dots
+########## Event Stuff ##########
 
 check = gp.Checkbox(app, 'See password') # A checkbox to toggle the password
 check.add_event_listener('change', toggle_mask) # Changes the password to a visible input
 
+#################################
+
+
+############  WIDGETS  ###############
+
+intro_lbl = gp.StyleLabel(app, "Password Checker 9000") # Label
+intro_lbl.font_size = 20 # Font size
+password_lbl = gp.Label(app, "Enter your password: ") # Label
+password_inp = gp.Secret(app) # Makes the input dots
+password_inp.width = 50 # Sets the size of the input
+
+#######################################
+
 app.add(intro_lbl, 1, 2, align="center") # Needed to show stuff on the app
-app.add(password_lbl, 3, 1) # Needed to show stuff on the app
-app.add(password_inp, 3, 2)
-app.add(check, 3, 3)
+app.add(password_lbl, 2, 1, align="center") # Aligns the input to the center
+app.add(password_inp, 2, 2) 
+app.add(check, 2, 3, align="center")
+app.add()
 
 app.run() # Makes the app run
