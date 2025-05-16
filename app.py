79f41760b554b830affc95f168fe5e743c7a4e50
OLLIE <onunn@student.saintaug.nsw.edu.au>
2025-05-16 12:04:18 +1000

    Made a basic password check to check for length, upper and lower case, if there is digits

--- app.py
+++ app.py
@@ -11,6 +11,20 @@ def toggle_mask(event): # Toggles the password
     password_inp.toggle() # Makes the input visible or not
 
 ######################
+############ Classes (def) ############
+
+def check_password(event): # Checks the password
+    password = password_inp.text # Gets the password
+    feedback.text = "" # Clears the feedback
+    if len(password) < 10: # Checks the length of the password
+        feedback.text += "Password is too short\n" # Adds to the feedback
+    if not any(char.isdigit() for char in password): # Checks if there are numbers in the password
+        feedback.text += "Password must contain a number\n" # Adds to the feedback
+    if not any(char.isupper() for char in password): # Checks if there are uppercase letters in the password
+        feedback.text += "Password must contain an uppercase letter\n" # Adds to the feedback
+    if not any(char.islower() for char in password): # Checks if there are lowercase letters in the password
+        feedback.text += "Password must contain a lowercase letter\n" # Adds to the feedback
+
 
 ######################
 # Need a score system for the password,
@@ -42,13 +56,20 @@ intro_lbl.font_size = 20 # Font size
 password_lbl = gp.Label(app, "Enter your password: ") # Label
 password_inp = gp.Secret(app) # Makes the input dots
 password_inp.width = 50 # Sets the size of the input
+feedback_lbl = gp.Label(app, "Feedback: ") # Label
+feedback = gp.Label(app, "") # Text box for feedback
 
 #######################################
+
+######## Adding Widgets to the app ########
 
 app.add(intro_lbl, 1, 2, align="center") # Needed to show stuff on the app
 app.add(password_lbl, 2, 1, align="center") # Aligns the input to the center
 app.add(password_inp, 2, 2) 
 app.add(check, 2, 3, align="center")
-app.add()
+app.add(feedback_lbl, 3, 1, align="center") # Aligns the input to the center
+app.add(feedback, 3, 2) # Adds the feedback input to the app
+
+###########################################
 
 app.run() # Makes the app run
