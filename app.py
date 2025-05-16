a9c593c9e4976fcefdc581385a02d417f9af1dba
OLLIE <onunn@student.saintaug.nsw.edu.au>
2025-05-16 11:47:18 +1000

    Added an idea for the password bar/progress score system to display the strength of the password

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gooeypie as gp
 
 WIDTH = 400
 HEIGHT = 300
-ROWS = 3
+ROWS = 4
 COLUMNS = 3
 
 ######  EVENTS  ######
@@ -10,6 +10,19 @@ COLUMNS = 3
 def toggle_mask(event): # Toggles the password
     password_inp.toggle() # Makes the input visible or not
 
+######################
+
+
+
+######################
+# Need a score system for the password,
+#                                       speacial characters = + points
+#                                       numbers = + points
+#                                       length 10 + = + points  
+#                                       mix of uppercase and lowercase = + points                                      
+#                                       whether or not it is a common password = +- points      
+#                                       if it has been pwnaed = - points
+#                                       common letters = - points
 ######################
 
 app = gp.GooeyPieApp("Password Checker") # Defines the stuff in app
@@ -21,7 +34,7 @@ intro_lbl.font_size = 20 # Font size
 password_lbl = gp.Label(app, "Enter your password") # Label
 password_inp = gp.Secret(app) # Makes the input dots
 
-check = gp.Checkbox(app, 'Toggle password') # A checkbox to toggle the password
+check = gp.Checkbox(app, 'See password') # A checkbox to toggle the password
 check.add_event_listener('change', toggle_mask) # Changes the password to a visible input
 
 app.add(intro_lbl, 1, 2, align="center") # Needed to show stuff on the app
